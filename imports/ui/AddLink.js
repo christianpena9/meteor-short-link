9df2c66b435b0bbe84c2607577f3541619c6298f
Christian Pena <christianpena9@gmail.com>
2017-07-10 23:35:51 -0400

    added a controlled input and checkbox

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -4,28 +4,46 @@ import { Meteor } from 'meteor/meteor';
 class AddLink extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            url: ''
+        };
         /*
             Below is a Custom Method binding 'this' so we don't have call it
             later on.
         */
         this.onSubmit = this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
     
     onSubmit(e) {
         e.preventDefault();
         
-        const url = this.refs.url.value.trim();
+        // const url = this.refs.url.value.trim();
+        // const url = this.state.url;
+        const { url } = this.state;
         
         if(url) {
-            Meteor.call('links.insert', url);
+            Meteor.call('links.insert', url, (err, res) => {
+                if(!err) {
+                    this.setState({ url: '' });
+                }
+            });
             
             /*
                 Below is old code that does not work any more since we removed
                 insecure.
              */
             //Links.insert({ url, userId: Meteor.userId() });
-            this.refs.url.value = '';
+            //this.refs.url.value = '';
         }
+    }
+    
+    onChange(e) {
+        this.setState({
+            // Added trim below since URL should not have spaces
+            // url: e.target.value.trim()
+            url: e.target.value
+        });
     }
     
     render() {
@@ -33,7 +51,14 @@ class AddLink extends React.Component {
             <div>
                 <p>Add Link</p>
                 <form onSubmit={this.onSubmit}>
-                    <input type="text" ref="url" placeholder="URL" autoComplete="off"/>
+                    <input
+                        type="text"
+                        //ref="url"
+                        placeholder="URL"
+                        autoComplete="off"
+                        value={this.state.url}
+                        onChange={this.onChange}
+                    />
                     <button>Add Link</button>
                 </form>
             </div>
