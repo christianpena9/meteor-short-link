15fc2c49c4b7e54003fa95fa8fcdcb00fb674673
Christian Pena <christianpena9@gmail.com>
2017-07-16 12:19:30 -0400

    added some styling to login and notfound page

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -49,7 +49,7 @@ class Login extends React.Component {
                         <input type="password" ref="password" name="password" placeholder="Password"/>
                         <button>Login</button>
                     </form>
-                    <Link to='/signup'>Need an account?</Link>
+                    <Link to="/signup">Need an account?</Link>
                 </div>
             </div>
         );
