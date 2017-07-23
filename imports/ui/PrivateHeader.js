fe6cfc541330c66bcfc0b0d51b39bdf3dbb2f502
Christian Pena <christianpena9@gmail.com>
2017-07-23 16:15:39 -0400

    Made the top nav look more presentable

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -36,9 +36,11 @@ const PrivateHeader = (props) => {
     // Below is another way and then call {onLogout} in the onClick
     // const onLogout = () => Accounts.logout();    
     return(
-        <div>
-            <h1>{props.title}</h1>
-            <button onClick={() => Accounts.logout()}>Logout</button>
+        <div className="header">
+            <div className="header__content">
+                <h1 className="header__title">{props.title}</h1>
+                <button className="button button--link-text" onClick={() => Accounts.logout()}>Logout</button>
+            </div>
         </div>
     );
 }
